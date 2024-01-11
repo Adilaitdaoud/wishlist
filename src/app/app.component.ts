8c7864a1849ff8553aa532d4e0271b99d53358ad
adilaitdaoud <adilaitdaoud2002@gmail.com>
2024-01-11 19:46:00 +0100

    using ng-template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,40 @@
-import { Component } from '@angular/core';
+import { Component,NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/models/wishItem';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  items:WishItem[]=[];
+  items:WishItem[]=[
+    new  WishItem('To Learn Angular'),
+    new  WishItem('Get  Coffee',true),
+    new WishItem('Commit the code')
+  ];
+  newWishText='new wish';
+
   title = 'wishlist';
+  clearFieald(){
+    this.newWishText='';
+  }
+  addNewWish(){
+    //todo:add wish to items array
+    //clear the text box
+    this.items.push(new WishItem(this.newWishText));
+    this.newWishText='new wish';
+    
+
+  }
+  toggleItem(item:WishItem){
+    item.isComplete = !item.isComplete;
+    console.log(item);
+  }
 }
