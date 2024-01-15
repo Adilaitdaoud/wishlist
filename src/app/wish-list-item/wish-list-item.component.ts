59e6b2b2da4984d39ae5e21c962f1bcef715b3f5
adilaitdaoud <adilaitdaoud2002@gmail.com>
2024-01-15 14:35:52 +0100

    Manage ccs classes with ngClass

--- src/app/wish-list-item/wish-list-item.component.ts
+++ src/app/wish-list-item/wish-list-item.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component,OnInit,Output,EventEmitter, Input } from '@angular/core';
 
 
 @Component({
   selector: 'app-wish-list-item',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './wish-list-item.component.html',
   styleUrl: './wish-list-item.component.css'
 })
@@ -14,8 +15,17 @@ export class WishListItemComponent implements OnInit {
   @Input()fullfilled!:boolean;
   @Output() fullfilledChange=new EventEmitter<boolean>();
 
-  
+  get cssClasses(){
+    //return this.fullfilled?['strikeout','text-muted']:[''];
+    //return {'strikeout':this.fullfilled,'text-muted':this.fullfilled};
+    return {'strikeout text-muted':this.fullfilled};
+  }
+
   ngOnInit(): void {
+  }
+  toggleFullfilled(){
+    this.fullfilled=!this.fullfilled;
+    this.fullfilledChange.emit(this.fullfilled);
   }
 
 }
