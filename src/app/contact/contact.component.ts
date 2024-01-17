9731440da91d0ea6f0ea436dde5f3698e78137d9
adilaitdaoud <adilaitdaoud2002@gmail.com>
2024-01-17 22:17:33 +0100

    configur the form control

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  ngOnInit(): void {
+  }
 
+  SenderNameControl=new FormControl('');
+  SenderEmailControl=new FormControl('');
+  SenderMessageControl=new FormControl('');
+
+  submitForm(){
+    if(this.SenderNameControl.dirty){
+      alert('you changed the name field');
+    }
+  }
 }
